f73e0a91b5fdf8939b9f3502260be9625fd86111
Valeri Ivanov Dimitrov <vid230@aubg.edu>
2026-09-13 14:46:27 +0300

    Added a folder Models with a file Course.cs for a course model - work on that new file is complete.

--- Project/AubgAcademicTracker/obj/Debug/net8.0/AubgAcademicTracker.AssemblyInfo.cs
+++ Project/AubgAcademicTracker/obj/Debug/net8.0/AubgAcademicTracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AubgAcademicTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c168f37ac7ed010f6c5f98e418d42149b98fc797")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0a788d84eb8f8227d8fd14472d3c7fb004ca883")]
 [assembly: System.Reflection.AssemblyProductAttribute("AubgAcademicTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AubgAcademicTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
