44672376d26f13d61efb0d84860e7b790fab9fa8
Valeri Ivanov Dimitrov <vid230@aubg.edu>
2026-09-16 16:04:49 +0300

    Dearchived everything to start work again.

---
+++ Project/AubgAcademicTracker/Program.cs
@@ -0,0 +1,30 @@
+using AubgAcademicTracker.Components;
+using AubgAcademicTracker.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddScoped<CourseService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
---
+++ Project/AubgAcademicTracker/Services/CourseService.cs
@@ -0,0 +1,20 @@
+﻿using AubgAcademicTracker.Models;
+
+namespace AubgAcademicTracker.Services
+{
+    public class CourseService
+    {
+        private readonly List<Course> courses = new();
+
+        private int nextId = 1;
+
+        public IReadOnlyList<Course> Courses => courses;
+
+        public void AddCourse(Course course)
+        {
+            course.Id = nextId++;
+
+            courses.Add(course);
+        }
+    }
+}
