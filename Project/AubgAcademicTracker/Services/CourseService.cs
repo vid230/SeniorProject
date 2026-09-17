f1019096eba8bb59be49e037d5d678e220874eed
Valeri Ivanov Dimitrov <vid230@aubg.edu>
2026-09-17 19:50:59 +0300

    Changed CourseService.cs.

--- Project/AubgAcademicTracker/Services/CourseService.cs
+++ Project/AubgAcademicTracker/Services/CourseService.cs
@@ -1,20 +1,32 @@
-﻿using AubgAcademicTracker.Models;
+﻿using AubgAcademicTracker.Data;
+using AubgAcademicTracker.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace AubgAcademicTracker.Services
 {
     public class CourseService
     {
-        private readonly List<Course> courses = new();
+        private readonly IDbContextFactory<AcademicDbContext> contextFactory;
 
-        private int nextId = 1;
-
-        public IReadOnlyList<Course> Courses => courses;
-
-        public void AddCourse(Course course)
+        public CourseService(IDbContextFactory<AcademicDbContext> contextFactory)
         {
-            course.Id = nextId++;
+            this.contextFactory = contextFactory;
+        }
 
-            courses.Add(course);
+        public async Task<List<Course>> GetCoursesAsync()
+        {
+            await using var context = await contextFactory.CreateDbContextAsync();
+
+            return await context.Courses.AsNoTracking().OrderBy(course => course.Id).ToListAsync();
+        }
+
+        public async Task AddCourseAsync(Course course)
+        {
+            await using var context = await contextFactory.CreateDbContextAsync();
+
+            context.Courses.Add(course);
+
+            await context.SaveChangesAsync();
         }
     }
 }
