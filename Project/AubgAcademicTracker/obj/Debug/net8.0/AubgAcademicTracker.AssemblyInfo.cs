97a5d930efdb0dfacd2cb7700574c6778cedd8a4
Valeri Ivanov Dimitrov <vid230@aubg.edu>
2026-09-17 19:11:36 +0300

    Installed three packages for the third stage of my project.

--- Project/AubgAcademicTracker/obj/Debug/net8.0/AubgAcademicTracker.AssemblyInfo.cs
+++ Project/AubgAcademicTracker/obj/Debug/net8.0/AubgAcademicTracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AubgAcademicTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44672376d26f13d61efb0d84860e7b790fab9fa8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15620c2601e77597bc57bd54d237f718a8488ae7")]
 [assembly: System.Reflection.AssemblyProductAttribute("AubgAcademicTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AubgAcademicTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
