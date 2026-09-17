a8e18375b3f53e32bf632402d0094a8a90c60d48
Valeri Ivanov Dimitrov <vid230@aubg.edu>
2026-09-17 19:24:38 +0300

    Adding a new folder with new class and adding to the json file.

--- Project/AubgAcademicTracker/obj/Debug/net8.0/AubgAcademicTracker.AssemblyInfo.cs
+++ Project/AubgAcademicTracker/obj/Debug/net8.0/AubgAcademicTracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AubgAcademicTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15620c2601e77597bc57bd54d237f718a8488ae7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97a5d930efdb0dfacd2cb7700574c6778cedd8a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("AubgAcademicTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AubgAcademicTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
