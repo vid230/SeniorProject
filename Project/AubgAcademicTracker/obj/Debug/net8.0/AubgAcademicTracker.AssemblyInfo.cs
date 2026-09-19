083f57e3a2c17d9f543abbe650f680785829428d
Valeri Ivanov Dimitrov <vid230@aubg.edu>
2026-09-19 18:30:49 +0300

    Added an EditCourse.razor file in the Pages folder and began working on it.

--- Project/AubgAcademicTracker/obj/Debug/net8.0/AubgAcademicTracker.AssemblyInfo.cs
+++ Project/AubgAcademicTracker/obj/Debug/net8.0/AubgAcademicTracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AubgAcademicTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+272f5a14ea8c87fae93506be6d60a44b76abb409")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16feaa2e8eb5149c2b85dd78c2e73ab8ffd182a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("AubgAcademicTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AubgAcademicTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
