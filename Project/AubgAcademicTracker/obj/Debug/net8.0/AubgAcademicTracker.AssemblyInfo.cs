e12b3768dfa5fcdd20cb6bc8071a707745770b8b
Valeri Ivanov Dimitrov <vid230@aubg.edu>
2026-09-20 18:38:05 +0300

    Created a new GpaCalculator.cs file in the Services folder.

--- Project/AubgAcademicTracker/obj/Debug/net8.0/AubgAcademicTracker.AssemblyInfo.cs
+++ Project/AubgAcademicTracker/obj/Debug/net8.0/AubgAcademicTracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AubgAcademicTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e05eedd11d91644529f8ae8a6dd483b67f6cb555")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a0c11f7fa948f846d211fa8370dda9f2125b468")]
 [assembly: System.Reflection.AssemblyProductAttribute("AubgAcademicTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AubgAcademicTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
