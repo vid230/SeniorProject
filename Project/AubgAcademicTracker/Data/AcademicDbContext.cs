e05eedd11d91644529f8ae8a6dd483b67f6cb555
Valeri Ivanov Dimitrov <vid230@aubg.edu>
2026-09-20 17:39:22 +0300

    Changed the AcademicDbContext.cs file.

--- Project/AubgAcademicTracker/Data/AcademicDbContext.cs
+++ Project/AubgAcademicTracker/Data/AcademicDbContext.cs
@@ -8,5 +8,20 @@ namespace AubgAcademicTracker.Data
         public AcademicDbContext(DbContextOptions<AcademicDbContext> options) : base(options) {}
 
         public DbSet<Course> Courses => Set<Course>();
+
+        public DbSet<Semester> Semesters => Set<Semester>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Semester>().HasMany(semester => semester.Courses).WithOne(course => course.Semester).HasForeignKey(course => course.SemesterId).OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Semester>().HasIndex(semester => new
+            {
+                semester.Term,
+                semester.Year
+            }).IsUnique();
+        }
     }
 }
