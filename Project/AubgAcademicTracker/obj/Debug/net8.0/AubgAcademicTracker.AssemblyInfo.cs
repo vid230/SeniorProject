11abb342154f055b424f360552d6a63fe93a9041
Valeri Ivanov Dimitrov <vid230@aubg.edu>
2026-09-21 20:01:36 +0300

    Began work on a new file - SemesterDetails.razor.

--- Project/AubgAcademicTracker/obj/Debug/net8.0/AubgAcademicTracker.AssemblyInfo.cs
+++ Project/AubgAcademicTracker/obj/Debug/net8.0/AubgAcademicTracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AubgAcademicTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c9bac7151229323fb0cefc22ce48297aca5ceff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4128b482384da0657c7b424878e07356381e5bfe")]
 [assembly: System.Reflection.AssemblyProductAttribute("AubgAcademicTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AubgAcademicTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
