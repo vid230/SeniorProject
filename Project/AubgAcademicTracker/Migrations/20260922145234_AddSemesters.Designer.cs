b5afbcc8dfd9a97ed6eddc21a7763e56fff9df74
Valeri Ivanov Dimitrov <vid230@aubg.edu>
2026-09-22 17:56:31 +0300

    Added a new migration.

--- Project/AubgAcademicTracker/Migrations/20260922145234_AddSemesters.Designer.cs
+++ Project/AubgAcademicTracker/Migrations/20260922145234_AddSemesters.Designer.cs
@@ -2,6 +2,7 @@
 using AubgAcademicTracker.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AubgAcademicTracker.Migrations
 {
     [DbContext(typeof(AcademicDbContext))]
-    partial class AcademicDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260922145234_AddSemesters")]
+    partial class AddSemesters
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.31");
--- Project/AubgAcademicTracker/obj/Debug/net8.0/AubgAcademicTracker.AssemblyInfo.cs
+++ Project/AubgAcademicTracker/obj/Debug/net8.0/AubgAcademicTracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AubgAcademicTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4128b482384da0657c7b424878e07356381e5bfe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+baaafd812f0cdc0bdf3629376651a2d5c1299223")]
 [assembly: System.Reflection.AssemblyProductAttribute("AubgAcademicTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AubgAcademicTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
