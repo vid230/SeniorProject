3d21008c126e47ae4216cece1676d81d6cde8ff5
Valeri Ivanov Dimitrov <vid230@aubg.edu>
2026-09-23 14:59:27 +0300

    Created a new file in the Services folder and began work on it - CreditCalculator.cs.

---
+++ Project/AubgAcademicTracker/Data/AcademicDbContext.cs
@@ -0,0 +1,27 @@
+﻿using AubgAcademicTracker.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AubgAcademicTracker.Data
+{
+    public class AcademicDbContext : DbContext
+    {
+        public AcademicDbContext(DbContextOptions<AcademicDbContext> options) : base(options) {}
+
+        public DbSet<Course> Courses => Set<Course>();
+
+        public DbSet<Semester> Semesters => Set<Semester>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Semester>().HasMany(semester => semester.Courses).WithOne(course => course.Semester).HasForeignKey(course => course.SemesterId).OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Semester>().HasIndex(semester => new
+            {
+                semester.Term,
+                semester.Year
+            }).IsUnique();
+        }
+    }
+}
---
+++ Project/AubgAcademicTracker/Services/CreditCalculator.cs
@@ -0,0 +1,22 @@
+﻿using AubgAcademicTracker.Models;
+
+namespace AubgAcademicTracker.Services
+{
+    public class CreditCalculator
+    {
+        public int CalculateAttemptedCredits(IEnumerable<Course> courses)
+        {
+            return courses.Sum(course => course.Credits);
+        }
+
+        public int CalculateEarnedCredits(IEnumerable<Course> courses)
+        {
+            return courses.Where(course => IsPassingGrade(course.Grade)).Sum(course => course.Credits);
+        }
+
+        public int CalculateCompletedCourses(IEnumerable<Course> courses)
+        {
+            return courses.Count(course => IsPassingGrade(course.Grade));
+        }
+    }
+}
