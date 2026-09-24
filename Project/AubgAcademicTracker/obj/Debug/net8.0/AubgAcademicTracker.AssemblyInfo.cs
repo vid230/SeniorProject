bef122f64b67b566ee75c75a0bb96d8c6bb4c548
Valeri Ivanov Dimitrov <vid230@aubg.edu>
2026-09-24 19:58:40 +0300

    Created and finished work on a new file in the Services folder - WhatIfCalculator.cs.

--- Project/AubgAcademicTracker/obj/Debug/net8.0/AubgAcademicTracker.AssemblyInfo.cs
+++ Project/AubgAcademicTracker/obj/Debug/net8.0/AubgAcademicTracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AubgAcademicTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a06595aed5ee3a999e8e05524a9b43e2b2d50bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e445cffedb29116075f07310c3d8fd8d1ece7e8c")]
 [assembly: System.Reflection.AssemblyProductAttribute("AubgAcademicTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AubgAcademicTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
