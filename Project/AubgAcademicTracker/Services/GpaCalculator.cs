3a06595aed5ee3a999e8e05524a9b43e2b2d50bd
Valeri Ivanov Dimitrov <vid230@aubg.edu>
2026-09-24 19:20:51 +0300

    Checked my own cumulative GPA with my program until now.

---
+++ Project/AubgAcademicTracker/Services/GpaCalculator.cs
@@ -0,0 +1,49 @@
+﻿using AubgAcademicTracker.Models;
+
+namespace AubgAcademicTracker.Services
+{
+    public class GpaCalculator
+    {
+        private readonly CreditCalculator creditCalculator;
+
+        public GpaCalculator(CreditCalculator creditCalculator)
+        {
+            this.creditCalculator = creditCalculator;
+        }
+
+        public double CalculateGpa(IEnumerable<Course> courses)
+        {
+            List<Course> courseList = courses.ToList();
+
+            int attemptedCredits = creditCalculator.CalculateAttemptedCredits(courseList);
+
+            if (attemptedCredits == 0)
+            {
+                return 0;
+            }
+
+            double qualityPoints = courseList.Sum(course => GetGradePoints(course.Grade) * course.Credits);
+
+            return qualityPoints / attemptedCredits;
+        }
+
+        public double GetGradePoints(string grade)
+        {
+            return grade switch
+            {
+                "A" => 4.0,
+                "A-" => 3.7,
+                "B+" => 3.3,
+                "B" => 3.0,
+                "B-" => 2.7,
+                "C+" => 2.3,
+                "C" => 2.0,
+                "C-" => 1.7,
+                "D+" => 1.3,
+                "D" => 1.0,
+                "F" => 0.0,
+                _ => throw new ArgumentException("Invalid grade.")
+            };
+        }
+    }
+}
